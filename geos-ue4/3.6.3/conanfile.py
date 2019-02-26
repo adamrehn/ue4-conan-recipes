1f999b48593cb9b98027334be74cd1d343fd62ab
Adam Rehn <adam.rehn@my.jcu.edu.au>
2019-02-26 21:34:36 +1000

    Remove hard-coded Conan cache paths in geos-config

--- geos-ue4/3.6.3/conanfile.py
+++ geos-ue4/3.6.3/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+import os
 
 class GeosUe4Conan(ConanFile):
     name = "geos-ue4"
@@ -41,6 +42,24 @@ class GeosUe4Conan(ConanFile):
             cmake.configure(source_folder="geos", args=self.cmake_flags())
             cmake.build()
             cmake.install()
+        
+        # We need to post-process `geos-config` on platforms where it is generated
+        geosConfig = os.path.join(self.package_folder, "bin", "geos-config")
+        if os.path.exists(geosConfig):
+            
+            # Allow the script to be written to
+            os.chmod(geosConfig, 0o777)
+            
+            # Patch the script to remove the hardcoded paths to the local Conan cache
+            script = tools.load(geosConfig)
+            script = "\n".join([
+                "#!/usr/bin/env bash",
+                "prefix=\"$( cd \"$( dirname \"${BASH_SOURCE[0]}\" )\" && cd .. && pwd )\"",
+                "exec_prefix=\"$prefix/bin\"",
+                "libdir=\"$prefix/lib\"",
+                "\n"
+            ]) + script[ script.index("usage()") : ]
+            tools.save(geosConfig, script)
     
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
