23b8d6393cf3bbd4698363399f2a1c2500830483
Adam Rehn <adam@adamrehn.com>
2020-07-24 17:02:23 +1000

    Add recipe for mergetiff

---
+++ mergetiff-ue4/0.0.2/conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile, CMake, tools
+
+class MergetiffUe4Conan(ConanFile):
+    name = "mergetiff-ue4"
+    version = "0.0.2"
+    license = "MIT"
+    url = "https://github.com/adamrehn/ue4-conan-recipes/mergetiff-ue4"
+    description = "mergetiff header-only package for Unreal Engine 4"
+    generators = "cmake"
+    
+    def source(self):
+        self.run("git clone --progress --depth=1 https://github.com/adamrehn/mergetiff-cxx.git -b v{}".format(self.version))
+    
+    def build(self):
+        
+        # Install the mergetiff headers
+        cmake = CMake(self)
+        cmake.configure(source_folder="mergetiff-cxx", args=["-DHEADER_ONLY=ON"])
+        cmake.install()
